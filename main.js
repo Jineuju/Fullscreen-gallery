6f2bb3d8968a8fb916fd627f1d5cd67372670d09
Jineuju <juj4628@gmail.com>
2023-04-17 12:50:20 +0900

    ver.1

---
+++ main.js
@@ -0,0 +1,38 @@
+let btns = document.querySelectorAll(".btns li");
+let hOne = document.querySelector("h1");
+let img = document.querySelector(".showBox img");
+let str = document.querySelector("strong");
+let sp = document.querySelector("span");
+
+let total = btns.length;
+
+sp.innerText = total;
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function (e) {
+        e.preventDefault(); //a태그가 가지고있는 링크 이벤트를 막는것
+
+        str.innerText = i + 1;
+        //strong태그에 클릭한 인덱스+1에 해당하는 숫자를 써줍니다
+
+        //클릭한 li안의 a태그에 있는 href속성값을 변수담습니다
+        let imgSrc = btns[i].querySelector("a").getAttribute("href");
+
+        //클릭한 li안의 a태그에 있는 text를 변수에 담아
+        let txt = btns[i].querySelector("a").innerText;
+        //h1안에 택스트와 대체합니다
+        hOne.innerText = txt;
+
+        //imgSrc에 변수로 담은 이미지 주소를 img의 src에 대체합니다
+        img.setAttribute("src", imgSrc);
+
+        //활성화 클래스 on을 이용해서 클릭한 버튼에 on을 붙여서 css를 변경해줍니다
+        //on을 모두 제거하고
+        for (let k = 0; k < btns.length; k++) {
+            btns[k].classList.remove("on");
+        }
+        btns[i].classList.add('on');
+        // 클릭한 대상에 on을 붙입니다
+
+    })
+}
